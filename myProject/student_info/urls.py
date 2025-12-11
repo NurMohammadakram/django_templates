bf398f749e2a7c5ae49cfa2d79d17d70b1617b24
Nur Mohammad Akram <nurmohammad19k8@gmail.com>
2025-12-11 13:51:41 +0600

    added create, show functionality

--- myProject/student_info/urls.py
+++ myProject/student_info/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    
+    path('/add_student', views.add_student, name='add student'),
+    path('/all_student', views.add_student, name='all student')
 ]
--- myProject/student_info/views.py
+++ myProject/student_info/views.py
@@ -1,9 +1,38 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.db.models import Q
+from student_info.models import Student
 # Create your views here.
 def home(request):
     return render(request, 'index.html')
 
-# def add_student(request):
-#     if request.method == "POST":
+def add_student(request):
+    if request.method == "POST":
+        Student.objects.create(
+            name = request.POST.get('name'),
+            email = request.POST.get('email'),
+            image = request.POST.get('image')
+        )
         
+        return redirect('all_student')
+    return render (request, 'student.add_student.html')
+
+def all_student(request):
+    query = request.GET.get(query)
+    studentData = Student.objects.all()
+    if query:
+        studentData = Student.objects.filter(
+            Q(name__icontains = query) | 
+            Q(email__icontains = query) 
+        )
+    
+    order = request.GET.get('order')
+    if order == 'asc':
+        studentData = Student.objects.order_by('name')
+    elif order == 'desc':
+        studentData = Student.objects.order_by('-name')
+    
+    context = {
+        "students": studentData
+    }
+    
+    return render(request, 'student.all_student.html', context)
